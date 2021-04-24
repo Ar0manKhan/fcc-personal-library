a9ae55d407cf4e970c727dd98c55b28ce808ab6c
Arman <arman7739a@gmail.com>
2021-04-24 09:05:11 +0530

    Completed backend api routes

--- routes/api.js
+++ routes/api.js
@@ -1,47 +1,73 @@
-/*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
-
 'use strict';
 
-module.exports = function (app) {
+module.exports = function (app, model) {
 
   app.route('/api/books')
-    .get(function (req, res){
+    .get(function (_req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      model.find({}, (err, data) => {
+        if (err) res.send("Some unexpected error occured, try again");
+        else res.send(data);
+      });
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
+      if (!title) {
+        res.send("missing required field title");
+        return;
+      }
+
+      const new_book = new model({ title });
+
+      new_book.save((err, data) => {
+        if (err) res.send("Some unexpected error occured, try again");
+        else res.json({ _id: data._id, title });
+      });
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (_req, res) {
       //if successful response will be 'complete delete successful'
+      model.deleteMany({}, (err, _data) => {
+        if (err) res.send("Some unexpected error occured, try again");
+        else res.send("complete delete successful");
+      });
     });
 
 
 
   app.route('/api/books/:id')
-    .get(function (req, res){
+    .get(function (req, res) {
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      model.findById(bookid, (err, data) => {
+        if (err || !data) res.send("no book exists");
+        else res.send(data);
+      });
     })
-    
-    .post(function(req, res){
+
+    .post(function (req, res) {
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
+      if (!comment) res.send("missing required field comment");
+      else model.findByIdAndUpdate(bookid, {
+        $inc: { commentcount: 1 },
+        $push: { comments: comment }
+      }, { new: true }, (err, data) => {
+        if (err || !data) res.send("no book exists");
+        else res.send(data);
+      });
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, _res) {
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
+      model.findByIdAndRemove(bookid, (err, data) => {
+        if (err || !data) res.send("no book exists");
+        else res.send("delete successful");
+      });
     });
-  
 };
--- server.js
+++ server.js
@@ -1,22 +1,36 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
 const app = express();
 
+// Connecting to database
+mongoose.connect(process.env.DB, {
+  useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false
+});
+
+// Creating schema to store books
+const { Schema } = mongoose;
+const book_schema = new Schema({
+  title: { type: String, required: true },
+  commentcount: { type: Number, default: 0 },
+  comments: [String]
+});
+const books = mongoose.model('books', book_schema);
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
+app.use(cors({ origin: '*' })); //USED FOR FCC TESTING PURPOSES ONLY!
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Index page (static HTML)
 app.route('/')
@@ -28,10 +42,10 @@ app.route('/')
 fccTestingRoutes(app);
 
 //Routing for API 
-apiRoutes(app);  
-    
+apiRoutes(app, books);
+
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
@@ -40,15 +54,15 @@ app.use(function(req, res, next) {
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         let error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
+        console.log('Tests are not valid:');
+        console.log(error);
       }
     }, 1500);
   }
